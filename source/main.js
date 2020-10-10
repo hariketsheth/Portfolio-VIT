555cdcfae987b7d33f9c8a7c08999ada3b47f46c
Hariket Sukeshkumar Sheth <72455881+hariketsheth@users.noreply.github.com>
2020-10-11 00:25:33 +0530

    Adding Fade Effect

--- source/main.js
+++ source/main.js
@@ -59,3 +59,53 @@ jQuery(document).ready(function($) {
 
 	 var ThumbailsWidth = ($('#image-slider').width() - 18.5)/7;
         $('#thumbnail li').find('img').css('width', ThumbailsWidth);
+
+	
+	 $(".seq-preloader").fadeOut(); 
+        $(".sequence").delay(500).fadeOut("slow"); 
+            
+        
+        $(function() {
+  
+        function showSlide(n) {
+     
+          
+            $body.unbind("mousewheel");
+          
+            currSlide = Math.min(Math.max(0, currSlide + n), $slide.length-1);
+            
+            var displacment = window.innerWidth*currSlide;
+         
+            $slides.css('transform', 'translateX(-' + displacment + 'px)');
+         
+            setTimeout(bind, 700);
+            
+            $('nav a.active').removeClass('active');
+            $($('a')[currSlide]).addClass('active');
+            
+        }
+      
+        function bind() {
+             $body.bind('false', mouseEvent);
+          }
+      
+        function mouseEvent(e, delta) {
+      
+            showSlide(delta >= 0 ? -1 : 1);
+            e.preventDefault();
+        }
+        
+        $('nav a, .main-btn a').click(function(e) {
+     
+            var newslide = parseInt($(this).attr('href')[1]);
+
+            var diff = newslide - currSlide - 1;
+            showSlide(diff); 
+            e.preventDefault();
+        });
+      
+        $(window).resize(function(){
+     
+          var displacment = window.innerWidth*currSlide;
+          $slides.css('transform', 'translateX(-'+displacment+'px)');
+        });
